c60b9941ec65c354fa5cb918b1c9ce190726676b
barlydjaja <barlydjaja@gmail.com>
2021-02-22 21:12:12 +0700

    Added login api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -30,3 +30,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+    
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+            
+        return super().update(instance, validated_data)
